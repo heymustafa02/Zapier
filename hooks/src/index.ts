0ff09d1d29f28fe88bc68f7b0286428caf4367d8
heymustafa02 <heymustafa.work@gmail.com>
2025-10-25 17:28:45 +0530

    Init

---
+++ hooks/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
+  const body = req.body;
+
+  
+      // store in db a new trigger
+
+});
